569e64506213585f4f0f368bba83e2d6edf81f0a
Ben-Langdon-VT <benplangdon@gmail.com>
2023-10-25 18:27:57 -0400

    test

---
+++ Unit_3/Classes/classes.js
@@ -0,0 +1,19 @@
+//! Classes
+/* 
+    - Introduce in 2015
+    - A function that helps us create objects
+    - defines a category of objects
+
+    * Base Structure
+
+    class NameOfClass {
+        constructor(parameter){
+            this.key = parameter;
+        }
+
+        methodName(){
+            ...code block
+        }
+    }
+    
+*/
